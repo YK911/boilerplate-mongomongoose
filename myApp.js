31b07fc4d3084c7d976090fb6ba667719f3abe9f
yaroslav kosytsia <jaroslavkositsa@gmail.com>
2024-10-18 22:16:29 +0000

    solve task 2

--- myApp.js
+++ myApp.js
@@ -9,7 +9,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.error("Connection to DB failed:", err);
   })
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name:  String,
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
